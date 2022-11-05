cb7e6f026a19dba6436b75a8b29614cf03b5672d
AP8673 <babaabhishek86@gmail.com>
2022-11-05 20:14:26 +0530

    worked on Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+
+
+import "./styles/App.scss"
+import "./styles/header.scss"
+import "./styles/home.scss"
 
 const  App = () => {
   return (
     <Router>
-        <Routes>
-          <Route />
-        </Routes>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
     </Router>
   );
 }
